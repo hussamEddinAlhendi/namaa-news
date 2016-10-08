da8b96c31cc42d398b71e7829acb2890c5fca0a5
HussamEddinAlhendi <housam35@gmail.com>
2016-10-09 02:12:03 +0300

    Implement news functionality and add UI

--- client/app/news/news.js
+++ client/app/news/news.js
@@ -1,11 +1,86 @@
 angular.module('namaa-news.news', [])
 
-.controller('NewsController', function ($scope) {
-	$scope.news = [
-		{title: "Google Pixel phones and Home speaker take on Siri and Echo", overview: "Google has placed a virtual assistant at the heart of its latest smartphones and first voice-activated speaker.", details: "http://www.bbc.com/news/technology-37551413", date: "10/5/2016"},
-		{title: "Google Pixel phones and Home speaker take on Siri and Echo", overview: "Google has placed a virtual assistant at the heart of its latest smartphones and first voice-activated speaker.", details: "http://www.bbc.com/news/technology-37551413", date: "10/5/2016"},
-		{title: "Google Pixel phones and Home speaker take on Siri and Echo", overview: "Google has placed a virtual assistant at the heart of its latest smartphones and first voice-activated speaker.", details: "http://www.bbc.com/news/technology-37551413", date: "10/5/2016"},
-		{title: "Google Pixel phones and Home speaker take on Siri and Echo", overview: "Google has placed a virtual assistant at the heart of its latest smartphones and first voice-activated speaker.", details: "http://www.bbc.com/news/technology-37551413", date: "10/5/2016"}
+.controller('NewsController', function ($scope, $mdDialog, News) {
+	$scope.initialize = function () {
+		News.getAllNews().then(function (resp) {
+			$scope.news = resp.data;
+		});
+	};
 
-	]
+	$scope.addNewNews = function () {
+		News.addNew($scope.new).then(function (resp) {
+			console.log(resp);
+			$scope.initialize();
+		});
+	};
+
+	$scope.deleteNews = function (id) {
+		News.deleteNews(id).then(function (resp) {
+			console.log(resp);
+			$scope.initialize();
+		});
+	};
+
+	$scope.editNews = function (news) {
+		News.editNews(news).then(function (resp) {
+			console.log(resp);
+			$scope.initialize();
+		});
+	};
+
+	$scope.showDialog = function(ev, news) {
+		$mdDialog.show({
+			locals: {news: news},
+			controller: editNews,
+			templateUrl: 'app/news/editNews.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:true,
+			fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+		})
+		.then(function(news) {
+			$scope.editNews(news)
+		}, function() {
+			console.log('cancel');
+		});
+	};
+
+	function editNews($scope, $mdDialog, news) {
+		console.log(news)
+		$scope.news = news;
+	    $scope.hide = function() {
+	      $mdDialog.hide();
+	    };
+
+	    $scope.cancel = function() {
+	      $mdDialog.cancel();
+	    };
+
+	    $scope.edit = function(news) {
+	      $mdDialog.hide(news);
+	    };
+	}
+
+	$scope.showDetails = function(ev, news) {
+		$mdDialog.show({
+			locals: {news: news},
+			controller: details,
+			templateUrl: 'app/news/newsDetails.html',
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose:true,
+			fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+		})
+	};
+
+	function details($scope, $mdDialog, news) {
+		$scope.news = news;
+	    $scope.hide = function() {
+	      $mdDialog.hide();
+	    };
+
+	    $scope.cancel = function() {
+	      $mdDialog.cancel();
+	    };
+	}
 });
