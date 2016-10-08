5d734ac419c46193155f8b49fe697bee49fa938a
HussamEddinAlhendi <housam35@gmail.com>
2016-10-09 02:15:41 +0300

    add news routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,19 @@
 var users = require('../users/usersController.js');
+var news = require('../news/newsController.js');
 
 module.exports = function (app, express) {
+
+	// users routes
 	app.get('/api/users', users.getAll);
 	app.post('/api/users', users.addNewUser);
-	app.put('/api/users/', users.editUser)
+	app.put('/api/users/', users.editUser);
 	app.delete('/api/users/:username', users.deleteUser);
+
+	// news routes
+	app.get('/api/news', news.getAll);
+	app.post('/api/news', news.addNew);
+	app.put('/api/news', news.editNews);
+	app.delete('/api/news/:id', news.deleteNews);
+
 };
 
--- server/users/usersController.js
+++ server/users/usersController.js
@@ -1,41 +1,31 @@
 var db = require('../config/database.js')();
 
-var query = function(sql, res){
-	db.query(sql, function(err,rows){
-			if(err) throw err;
-			console.log(JSON.stringify(rows));
-			res.json(rows)
-		});
+var query = function(sql, values, res){
+	db.query(sql, values, function(err,rows){
+		if(err) throw err;
+		// console.log(JSON.stringify(rows));
+		res.json(rows)
+	});
 }
 
 
 module.exports = {
 	getAll: function (req, res, next) {
 		var sql = 'SELECT * FROM users';
-		query(sql, res)
+		query(sql, {}, res)
 	},
 	addNewUser: function (req, res, next) {
-		var sql = 'INSERT INTO users VALUES ("' + req.body.username + '", "' + req.body.password + '", "' + req.body.email + '")';
-		query(sql, res)
+		var sql = 'INSERT INTO users SET ?';
+		var user = {username: req.body.username, password: req.body.password, email: req.body.email};
+		query(sql, user, res);
 	},
 	deleteUser: function (req, res, next) {
-		console.log('asdfasdfadfasdfasfdasf', req.params)
-		var sql = 'DELETE FROM users WHERE username="' + req.params.username + '";'
-		query(sql, res)
+		var sql = 'DELETE FROM users WHERE username=?'
+		query(sql, [req.params.username], res);
 	},
 	editUser: function (req, res, next) {
-		if (!!req.body.password && !!req.body.email) {
-			var sql = 'UPDATE users SET password="' + req.body.password + '",email="' + req.body.email + '" WHERE username="' + req.body.username + '";';
-			console.log(sql)
-			query(sql, res)
-		} else if (!!req.body.password) {
-			var sql = 'UPDATE users SET password="' + req.body.password + '" WHERE username="' + req.body.username + '";';
-			console.log(sql)
-			query(sql, res)
-		} else if (!!req.body.email) {
-			var sql = 'UPDATE users SET email="' + req.body.email + '" WHERE username="' + req.body.username + '";';
-			console.log(sql)
-			query(sql, res)
-		}
+		var sql = 'UPDATE users SET password = ?, email = ?  WHERE username = ?';
+		var user = [req.body.password, req.body.email, req.body.username];
+		query(sql, user, res);
 	}
 };
